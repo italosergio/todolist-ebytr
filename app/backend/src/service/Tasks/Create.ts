d69f16d51dcce39899ecc05a5555a878f599f976
Italo Sergio <italosergio@graduate.org>
2022-07-02 15:32:37 -0300

    refactor: insert task

--- app/backend/src/service/Tasks/Create.ts
+++ app/backend/src/service/Tasks/Create.ts
@@ -4,7 +4,7 @@ import { Tasks } from '../../model';
 export default class Create {
   // eslint-disable-next-line class-methods-use-this
   public async Task(values: ITask): Promise<void> {
-    const insertTask = new Tasks();
-    await insertTask.create(values);
+    const tasks = new Tasks();
+    await tasks.create(values);
   }
 }
