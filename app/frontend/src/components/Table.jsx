edb09b44d438b5423b500982cc718ff963b10caa
Italo Sergio <italosergio@graduate.org>
2022-07-04 03:19:53 -0300

    feat: can update one each priority task

--- app/frontend/src/components/Table.jsx
+++ app/frontend/src/components/Table.jsx
@@ -148,9 +148,42 @@ const Table = ({
                   </td>
                   <td>{datePhrase}</td>
                   <td>
-                    {task.priority === 1 ? <div class="ui green button"></div> : null}
-                    {task.priority === 2 ? <button class="ui yellow button"></button> : null}
-                    {task.priority === 3 ? <button class="ui red button"></button> : null}
+                    {
+                      task.priority === 1
+                      ? <div
+                          id={task.id}
+                          class="ui green button"
+                          onClick={({ target: { id } }) => updateEachTask({ id, editedTask: { priority: 2 } })}
+                        />
+                        : null
+                    }
+                    {
+                      task.priority === 2
+                      ? <div
+                          id={task.id}
+                          class="ui yellow button"
+                          onClick={({ target: { id } }) => updateEachTask({ id, editedTask: { priority: 3 } })}
+                        />
+                        : null
+                    }
+                    {
+                      task.priority === 3
+                      ? <div
+                          id={task.id}
+                          class="ui red button"
+                          onClick={({ target: { id } }) => updateEachTask({ id, editedTask: { priority: 0 } })}
+                        />
+                        : null
+                    }
+                    {
+                      task.priority === 0
+                      ? <div
+                          id={task.id}
+                          class="ui basic button"
+                          onClick={({ target: { id } }) => updateEachTask({ id, editedTask: { priority: 1 } })}
+                        />
+                        : null
+                    }
                   </td>
                 </tr>
               )
