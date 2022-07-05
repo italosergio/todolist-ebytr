edbe50bc87bc72263ee1afc184bd030a5c35c285
Italo Sergio <italosergio@graduate.org>
2022-07-05 04:49:43 -0300

    test: DELETE /tasks

--- app/backend/src/tests/Unit.test.ts
+++ app/backend/src/tests/Unit.test.ts
@@ -239,3 +239,64 @@ describe('3 - PATCH /tasks', () => {
     expect(response.body.tasks[4].date).to.be.an('string');
   });
 });
+
+describe('4 - DELETE /tasks/:id', () => {
+  before(async () => {
+    sinon
+      .stub(tasksModel, "destroy")
+      .resolves();
+      
+    sinon
+      .stub(tasksModel, "findAll")
+      .resolves([...mockTasks] as unknown as tasksModel[]);
+
+    response = await chai
+      .request(app)
+      .post('/tasks')
+      .send({
+        task: {
+          description: "Nova tarefa",
+          date: "2022-07-03T04:20:20.000Z",
+          priority: 2
+        }
+      })
+
+    response = await chai
+      .request(app)
+      .delete('/tasks/5');
+  });
+
+  after(async () => {
+    sinon.restore();
+  })
+
+  it('Status code: 200', async () => {
+    expect(response.status).exist;
+    expect(response).to.have.status(200);
+  });
+
+  it('Response body com tarefa deletada', async () => {
+    expect(response.body.tasks).to.be.an('array');
+    expect(response.body.tasks).to.have.length(4);
+    expect(response.body.tasks[0].id).to.be.equal(1);
+    expect(response.body.tasks[0].description).to.be.equal('Reuniao Ebytr');
+    expect(response.body.tasks[0].priority).to.be.equal(3);
+    expect(response.body.tasks[0].status).to.be.equal(false);
+    expect(response.body.tasks[0].date).to.be.an('string');
+    expect(response.body.tasks[1].id).to.be.equal(2);
+    expect(response.body.tasks[1].description).to.be.equal('Daily com o time');
+    expect(response.body.tasks[1].priority).to.be.equal(2);
+    expect(response.body.tasks[1].status).to.be.equal(false);
+    expect(response.body.tasks[1].date).to.be.an('string');
+    expect(response.body.tasks[2].id).to.be.equal(3);
+    expect(response.body.tasks[2].description).to.be.equal('Sair com cachorro');
+    expect(response.body.tasks[2].priority).to.be.equal(0);
+    expect(response.body.tasks[2].status).to.be.equal(true);
+    expect(response.body.tasks[2].date).to.be.an('string');
+    expect(response.body.tasks[3].id).to.be.equal(4);
+    expect(response.body.tasks[3].description).to.be.equal('Beber Agua');
+    expect(response.body.tasks[3].priority).to.be.equal(1);
+    expect(response.body.tasks[3].status).to.be.equal(false);
+    expect(response.body.tasks[3].date).to.be.an('string');
+  });
+});
