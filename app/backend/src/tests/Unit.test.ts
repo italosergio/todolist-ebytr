754c033b4cacb11f802c60fb8f422f972e56d2c4
Italo Sergio <italosergio@graduate.org>
2022-07-05 04:32:25 -0300

    test: POST /tasks

--- app/backend/src/tests/Unit.test.ts
+++ app/backend/src/tests/Unit.test.ts
@@ -15,22 +15,22 @@ const { expect } = chai;
 let response: Response;
 
 describe('0 - Error middleware', async () => {
-    before(async () => {
-      response = await chai
+  before(async () => {
+    response = await chai
       .request(app)
       .post('/tasks')
       .send({
         sendNothing: ''
       });
-    })
+  })
 
-    it('Status code: 500', async () => {
-      expect(response).to.have.status(500);
-    })
+  it('Status code: 500', async () => {
+    expect(response).to.have.status(500);
+  })
 
-    it('Message: \'Something whrong!\'', async () => {
-      expect(response.body.message).to.be.equal('Something wrong!');
-    })
+  it('Message: \'Something whrong!\'', async () => {
+    expect(response.body.message).to.be.equal('Something wrong!');
+  })
 })
 
 describe('1 - GET /tasks', () => {
@@ -44,7 +44,7 @@ describe('1 - GET /tasks', () => {
       .get('/tasks');
   });
 
-  after(async ()=>{
+  after(async () => {
     sinon.restore();
   })
 
@@ -77,7 +77,7 @@ describe('1 - GET /tasks', () => {
     expect(response.body.tasks[3].status).to.be.equal(false);
     expect(response.body.tasks[3].date).to.be.an('string');
   });
-  
+
   it('Status code: 400', async () => {
     sinon.restore();
 
@@ -91,5 +91,73 @@ describe('1 - GET /tasks', () => {
 
     expect(response.status).exist;
     expect(response).to.have.status(400);
+  });
+});
+
+describe('2 - POST /tasks', () => {
+  before(async () => {
+    sinon
+      .stub(tasksModel, "findAll")
+      .resolves([...mockTasks, {
+        id: 5,
+        description: "Nova tarefa",
+        priority: 2,
+        status: false,
+        date: "2022-07-03T04:20:20.000Z",
+      }] as unknown as tasksModel[]);
+
+    sinon
+      .stub(tasksModel, "create")
+      .resolves();
+
+    response = await chai
+      .request(app)
+      .post('/tasks')
+      .send({
+        task: {
+          description: "Nova tarefa",
+          date: "2022-07-03T04:20:20.000Z",
+          priority: 2
+        }
+      })
+  });
+
+  after(async () => {
+    sinon.restore();
+  })
+
+  it('Status code: 201', async () => {
+    expect(response.status).exist;
+    expect(response).to.have.status(201);
+  });
+
+  it('Response body com nova tarefa adicionada', async () => {
+    expect(response.body.tasks).to.be.an('array');
+    expect(response.body.tasks).to.have.length(5);
+    expect(response.body.tasks[0].id).to.be.equal(1);
+    expect(response.body.tasks[0].description).to.be.equal('Reuniao Ebytr');
+    expect(response.body.tasks[0].priority).to.be.equal(3);
+    expect(response.body.tasks[0].status).to.be.equal(false);
+    expect(response.body.tasks[0].date).to.be.an('string');
+    expect(response.body.tasks[1].id).to.be.equal(2);
+    expect(response.body.tasks[1].description).to.be.equal('Daily com o time');
+    expect(response.body.tasks[1].priority).to.be.equal(2);
+    expect(response.body.tasks[1].status).to.be.equal(false);
+    expect(response.body.tasks[1].date).to.be.an('string');
+    expect(response.body.tasks[2].id).to.be.equal(3);
+    expect(response.body.tasks[2].description).to.be.equal('Sair com cachorro');
+    expect(response.body.tasks[2].priority).to.be.equal(0);
+    expect(response.body.tasks[2].status).to.be.equal(true);
+    expect(response.body.tasks[2].date).to.be.an('string');
+    expect(response.body.tasks[3].id).to.be.equal(4);
+    expect(response.body.tasks[3].description).to.be.equal('Beber Agua');
+    expect(response.body.tasks[3].priority).to.be.equal(1);
+    expect(response.body.tasks[3].status).to.be.equal(false);
+    expect(response.body.tasks[3].date).to.be.an('string');
+    expect(response.body.tasks[4].id).to.be.equal(5);
+    expect(response.body.tasks[4].description).to.be.equal('Nova tarefa');
+    expect(response.body.tasks[4].priority).to.be.equal(2);
+    expect(response.body.tasks[4].status).to.be.equal(false);
+    expect(response.body.tasks[4].date).to.be.an('string');
   });
 });
